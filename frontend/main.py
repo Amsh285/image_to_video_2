49d5b65ae3c4e60321974edea8cb03edf0f46653
Amsh285 <58710888+Amsh285@users.noreply.github.com>
2023-01-07 13:40:15 +0100

    save images

--- frontend/main.py
+++ frontend/main.py
@@ -1,10 +1,6 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, Response
 import requests
 import base64
-
-#app = Flask(__name__)
-#app.run(host='127.0.0.1', port=5001)
-#app.run(debug=True, port=8001)
 
 #flask --app main run
 #flask --app frontend/main run --port=5000
@@ -13,27 +9,34 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('imageupload.html')
+    return render_template('index.html')
 
 @app.route("/upload_images")
 def navigate_upload_images():
     return render_template("imageupload.html")
 
-@app.route("/save_images", methods=["post"])
-def process_images():
-    #print(request.files)
+@app.route("/download_images")
+def navigate_download_images():
+    return render_template("imagedownload.html")
 
-    #requests.post(url='http://127.0.0.1:5001/save_images', data=request.files.to_dict(flat=False))
-
-
-    #payload = {'ich': 'bin', 'extrem': 'dumm'}
+@app.route("/save_files", methods=["post"])
+def save_images():
     payload = list()
 
     for file in request.files.getlist("imgs"):
         t = file.read()
         payload.append((file.filename, base64.b64encode(t)))
 
-    requests.post(url='http://127.0.0.1:5001/save_images', data=payload)
+    result = requests.post(url='http://127.0.0.1:5001/save_files', data=payload)
+    return result.text, result.status_code
 
-    return "tolo"
 
+@app.route("/load_files", methods=["get"])
+def load_files():
+    request_file_names = request.args.get("filenames")
+    terminator = request.args.get("terminator")
+
+    file_names = request_file_names.split(terminator)
+    print(file_names)
+
+    return "ois guad", 200
--- repository/main.py
+++ repository/main.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, Response
+import base64
+import os
+
 
 app = Flask(__name__)
 
@@ -8,16 +11,24 @@ app = Flask(__name__)
 def test():
     return "ahoi";
 
-@app.route("/save_images", methods=["post"])
-def upload_images():
-    #print(request.get_data())
-    print("ich bin wirklich dumm!")
-    #print(request.get_data(as_text=True))
-    print(request.form.to_dict())
+@app.route("/save_files", methods=["post"])
+def upload_files():
+    form_data = request.form.to_dict()
 
-    formdata = request.form.to_dict()
+    image_folder = os.path.join(app.instance_path, "upload_images")
+    os.makedirs(image_folder, exist_ok=True)
 
     for b in request.form.to_dict():
-        print(formdata[b])
+        byte_content = base64.b64decode(form_data[b])
 
-    return "hoi"
+        with open(os.path.join(image_folder, b), "wb") as binary_file:
+            binary_file.write(byte_content)
+
+    return {'message': 'File(s) saved successful.'}, 201
+
+@app.route("/load_images", methods={"get"})
+def download_files():
+    form_data = request.form.to_dict()
+    print(form_data)
+
+    return 200
