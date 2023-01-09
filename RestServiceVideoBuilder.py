452a925104a4321f0881b22e304b415584445282
Markus Goldgruber <ai22m005@technikum-wien.at>
2023-01-09 14:00:51 +0100

    update including Docker Image generation

--- RestServiceVideoBuilder.py
+++ RestServiceVideoBuilder.py
@@ -6,9 +6,9 @@ from video_builder_cv2 import build_video_from_pngs
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
-# GOTO: http://192.168.0.163:5000/rest/data/v1.0/json/en/generate/video/ to generate video
-# @app.route('/rest/data/v1.0/json/en/generate/video/', methods=['GET', 'POST'])
-@app.route('/', methods=['GET', 'POST'])
+#@app.route('/', methods=['GET', 'POST'])
+# GOTO: http://localhost:5000/rest/data/v1.0/json/en/generate/video/ to generate video
+@app.route('/rest/data/v1.0/json/en/generate/video/', methods=['GET', 'POST'])
 def generate_video():
 
     image_folder = 'mandy_pngs'
--- video_builder_cv2.py
+++ video_builder_cv2.py
@@ -8,7 +8,7 @@ def build_video_from_pngs(image_folder: str, video_name: str = "output.mp4"):
     frame = cv2.imread(os.path.join(image_folder, images[0]))
     height, width, layers = frame.shape
     
-    print("Video saved to:", os.getcwd())
+    #print("Video saved to:", os.getcwd())
 
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     video = cv2.VideoWriter(video_name, fourcc, 5, frameSize=(width,height))
